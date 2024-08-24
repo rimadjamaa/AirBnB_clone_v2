e655df1185a09cacbe52e0baab4c99a06be696ce
DJAMAA Ryma <88832599+rimadjamaa@users.noreply.github.com>
2024-08-24 21:13:24 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,5 +12,10 @@ def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_hbnb():
+    """Returns a string at the specific route"""
+    return "Hello HBNB!"
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
